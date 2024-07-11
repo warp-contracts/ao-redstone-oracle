a2332e36c26239fc70d2d8c6206c34a4f91234a1
ppedziwiatr <ppedziwiatr@gmail.com>
2024-07-11 22:30:16 +0200

    validator process update

--- redstone-oracle-process/relayer.mjs
+++ redstone-oracle-process/relayer.mjs
@@ -14,7 +14,7 @@ const DATA_FEEDS = ["BTC", "ETH", "USDC", "USDT", "SOL", "stETH", "AR"];
 const DATA_SERVICE_ID = "redstone-primary-prod";
 const PROCESS = env == 'local'
   ? '5wL4f2Pi9dAEGOjanCoLFlxL0w3M3jI9AJcvRu89A_0'
-  : 'iSxR0exVxlpL26emdzNAa5YLvuL5aqDJX8tz5QiESS4';
+  : '_b21c1djDesKI5LPXBZvZbXKdkTgQIx2FsN2HXtFsqQ';
 
 const WALLET = JSON.parse(fs.readFileSync("./.secrets/wallet.json", "utf-8"));
 const MU_URL = env === 'local' ? 'http://localhost:8080' : 'https://mu.warp.cc';

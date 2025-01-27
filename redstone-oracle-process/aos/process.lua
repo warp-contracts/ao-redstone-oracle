101cd18b7b9562dba3c6a148c3b0cb9f9e374e9d
Tadeuchi <tadeuszwl@gmail.com>
2025-01-27 13:23:28 +0100

    fix: update _PricesAreMostRecent

--- redstone-oracle-process/aos/process.lua
+++ redstone-oracle-process/aos/process.lua
@@ -33,7 +33,9 @@ function _PricesAreMostRecent(priceData)
     local latestPrices = ORACLE.Storage[#ORACLE.Storage]
 
     for k, v in pairs(priceData) do
-        assert(v["verifiedPackage"].t > latestPrices[k]["verifiedPackage"].t, 'Only the most recent prices are stored')
+        if k ~= 'sentTs' then
+            assert(v["verifiedPackage"].t > latestPrices[k]["verifiedPackage"].t, 'Only the most recent prices are stored')
+        end
     end
 
     return true
